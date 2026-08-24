0cef3b0b2c015ad867844ddc9cc0bbd9c1f51eaf
Júlia Lourenço de Melo <julialourencodemelo@gmail.com>
2026-08-24 18:16:41 -0300

    Update Exe1.java

--- aeds2/Exe1/Exe1.java
+++ aeds2/Exe1/Exe1.java
@@ -15,7 +15,7 @@ public static void main (String [] args)
 {
 	Scanner sc= new Scanner ( System.in);
 	String f = sc.nextLine ();
-	while (!( f.length() == '3' && f.charAt(0) = 'F' && f.charAt(1) == 'I' && f.charAt(2) == 'M'))
+	while (!( f.length() == 3 && f.charAt(0) = 'F' && f.charAt(1) == 'I' && f.charAt(2) == 'M'))
 			{
 				cifrar(f);
 				f = sc.nextLine();
